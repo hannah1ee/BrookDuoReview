8c024516c943a74f4ebe9404d899f557ab5ed6b2
hannah1ee <hannahlee.code@gmail.com>
2024-03-07 09:06:56 -0800

    added review comments

--- grade.sh
+++ grade.sh
@@ -11,10 +11,15 @@ echo 'Finished cloning'
 if ! [ -f student-submission/ListExamples.java ]
 then 
     echo "Missing needed files"
+    # i like how you noted that there some needed files are missing 
+    # i think itd be good to specify that ListExamples.java specifically is missing
     exit 
 
 fi
 echo "continue"
+
+# i like how u put "continue" to communicate that the files are existing and that you know it'll now begin the grading
+
 cp student-submission/ListExamples.java TestListExamples.java grading-area 
 cp -r lib grading-area
 
@@ -23,9 +28,10 @@ javac -cp $CPATH *.java
 if [ $? -ne 0 ]
 then 
     echo "Compilation Error. Double check your code bugs or syntax"
+    # i like how you provide suggestions for where in your code something could be wrong
 fi 
 
-
+# i think it would be good if you are able to echo something about no errors passed
 
 
 # Draw a picture/take notes on the directory structure that's set up after
